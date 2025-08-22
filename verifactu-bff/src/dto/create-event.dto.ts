08627b4089f27f780a5651579d0242a8d065e723
mario <mrcompa@gmail.com>
2025-08-22 10:31:06 +0200

    chore: initial snapshot monorepo limpio

--- verifactu-bff/src/dto/create-event.dto.ts
+++
@@ -1,8 +0,0 @@
-import { EventType } from '../entities/event-log.entity';
-
-// Este es el "formulario" que el conector de Electron usará
-// para enviar la información de un nuevo evento.
-export class CreateEventDto {
-    eventType: EventType;
-    details: object;
-}
--- verifactu-printer-connector/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  getSettings: () => ipcRenderer.invoke('get-settings'),
-  openFolderDialog: () => ipcRenderer.invoke('open-folder-dialog'),
-  saveSettings: (settings) => ipcRenderer.invoke('save-settings', { folderPath: settings.folderPath }),
-});
